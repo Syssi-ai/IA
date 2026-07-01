f742b5b260c96191d1ab1acf3248be76c609871e
Syssi-ai <syrine.bouaziz1706@gmail.com>
2026-07-01 12:16:22 +0200

    numpy

---
+++ Partie_5/Partie_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+scores = np.array([88, 72, 95, 60, 83])
+
+print("Array:", scores)
+print("Type:", type(scores))
+print("Shape:", scores.shape)    # (5,) means 5 items in 1 dimension
