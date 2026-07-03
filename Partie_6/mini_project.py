9b3784543507713d21b4a5291e650f5710ef6d0c
Syssi-ai <syrine.bouaziz1706@gmail.com>
2026-07-03 11:33:23 +0200

    mon mini projet

--- Partie_6/mini_project.py
+++ Partie_6/mini_project.py
@@ -2,18 +2,18 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = Image.open("Veau.jpg")  #  Your turn! Try any effect you like.
+dessin = Image.open("Veau.jpg")  #  Your turn! Try any effect you like.
 
 def traitement_image(img):
-    img = img.resize(64,64)
+    img = img.resize((64, 64))
     img_gray = img.convert("L")    
     np_img = np.array(img_gray) / 255.0
     return np_img
 
-print(traitement_image(img))
-print("MIN:", traitement_image(img).min())
-print("MAX:", traitement_image(img).max())
-plt.imshow(traitement_image(img), cmap='gray')
+print(traitement_image(dessin))
+print("MIN:", traitement_image(dessin).min())
+print("MAX:", traitement_image(dessin).max())
+plt.imshow(traitement_image(dessin), cmap='gray')
 plt.axis('off')
 plt.tight_layout()
 plt.show()
