6f50e48b525d71d2117e6d847639c80bbd030f93
Syssi-ai <syrine.bouaziz1706@gmail.com>
2026-07-03 12:58:10 +0200

    setup

---
+++ Data_process/Setup.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+import os
+
+print("All libraries loaded! ✅")
+print("TensorFlow version:", tf.__version__)
